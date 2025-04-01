832d6a42f64d7d3f5920ce855b536bb09ffd2788
jarl1409 <jhosepandrey203@gmail.com>
2025-03-31 23:37:15 -0500

    cambiando imagen automotriz

--- pinturas-del-meta/src/components/Cards.tsx
+++ pinturas-del-meta/src/components/Cards.tsx
@@ -4,16 +4,18 @@ export default function Cards() {
       <div className="h-lvh">
         <div className="flex flex-col items-center justify-center p-4">
           {/* Contenedor principal de la tarjeta */}
-          <div className="relative w-fit h-fit max-w-md rounded-lg overflow-hidden shadow-lg">
+          <div className="relative w-fit h-fit max-w-md overflow-hidden shadow-lg">
             {/* Imagen de la tarjeta */}
             <img
-              src="/public/Automotriz.jpg"
+              src="/public/Automotriz.png"
               alt="Card"
               className="h-full aspect-square object-cover"
             />
             {/* Contenedor del texto superpuesto */}
-            <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent p-4 flex items-end justify-center ">
-              <h3 className="text-3xl font-semibold text-white ">Arquitectura</h3>
+            <div className="absolute inset-0 flex items-end justify-start">
+              <h3 className="text-3xl font-semibold text-white p-4">
+                Arquitectura
+              </h3>
             </div>
           </div>
         </div>
